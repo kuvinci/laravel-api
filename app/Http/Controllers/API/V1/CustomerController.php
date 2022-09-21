429a1da1069dfbdd209eb62ef8c736c4dfa80e60
kuvinci <kuvinci@gmail.com>
2022-09-21 23:25:56 +0300

    Data filtering with different operators

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -16,9 +18,16 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request) : CustomerCollection
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
+
+        if(count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+        } else {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
     }
 
     /**
---
+++ app/Services/V1/CustomerQuery.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services\V1;
+
+use Illuminate\Http\Request;
+
+class CustomerQuery {
+    protected array $allowedParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt']
+    ];
+
+    protected array $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    protected array $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+    ];
+
+    public function transform(Request $request) : array {
+        $eloQuery = [];
+
+        foreach ($this->allowedParams as $param => $operators) {
+            $query = $request->query($param);
+
+            if(!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach($operators as $operator) {
+                if(isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+
+        }
+
+        return $eloQuery;
+    }
+}
