9ab67c1b5c7e21dfd83d76d228feb2294b405069
kuvinci <kuvinci@gmail.com>
2022-09-23 17:21:01 +0300

    Adding PUT and PATCH status support *deleting unnecessary files*

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
@@ -65,13 +65,13 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Http\Requests\V1\UpdateCustomerRequest  $request
      * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
+     * @return void
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
+    public function update(UpdateCustomerRequest $request, Customer $customer) : void
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
