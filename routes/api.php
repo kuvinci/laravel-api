6217e251f0e7c8fd067c152d2a353d6b08ffd7bd
kuvinci <kuvinci@gmail.com>
2022-09-23 20:11:04 +0300

    Sanctum token protection of the API admin/update/basic Use /setup route to get your Bearer token

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // api/v1
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\API\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\API\V1', 'middleware' => 'auth:sanctum'], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 
